256e78ce21cc2ea62801193e11202925b431701d
fadi alhabib <fadi.b.alhabib@gmail.com>
2023-09-26 06:04:58 +0300

    returned old computer

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,24 +1,19 @@
-import React, { Suspense, useEffect, useMemo, useState } from 'react';
+import React, { Suspense, useEffect, useState } from 'react';
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
-import { useGLTF, Preload } from '@react-three/drei';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 import CanvasLoader from '../Loader';
 
 const Computers = ({ isMobile }) => {
-  const { scene, error } = useGLTF('./desktop_pc/scene.gltf');
-
-  if (error) {
-    // Handle the error (e.g., display an error message)
-    return <div>Error loading 3D model</div>;
-  }
+  const computer = useGLTF('./desktop_pc/scene.gltf');
 
   return (
-    <mesh frustumCulled={false}>
+    <mesh>
+      <hemisphereLight intensity={3.0} groundColor="blue" />
       <pointLight intensity={isMobile ? 0.5 : 1.5} color="blue" />
       <primitive
-        object={scene}
-        scale={isMobile ? [0.1, 0.1, 0.1] : [0.7, 0.7, 0.7]} // Adjust scale for mobile
-        position={isMobile ? [0, -0.2, -0.5] : [0, -3.25, -1.25]} // Adjust position for mobile
+        object={computer.scene}
+        scale={isMobile ? 0.25 : 0.7}
+        position={isMobile ? [0, -0.4, -0.5] : [0, -3.25, -1.25]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -29,7 +24,7 @@ const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
-    const mediaQuery = window.matchMedia('(max-width: 768px)'); // Adjust the max-width for your needs
+    const mediaQuery = window.matchMedia('(max-width:500px)');
     setIsMobile(mediaQuery.matches);
     const handleMediaQueryChange = (event) => {
       setIsMobile(event.matches);
@@ -40,32 +35,23 @@ const ComputersCanvas = () => {
     };
   }, []);
 
-  const cameraSettings = useMemo(
-    () => ({
-      position: isMobile ? [0, 1, 4] : [20, 3, 5], // Adjust camera position for mobile
-      fov: isMobile ? 45 : 25, // Adjust field of view for mobile
-    }),
-    [isMobile]
-  );
-
   return (
     <Canvas
       frameloop="demand"
-      camera={cameraSettings}
-      gl={{ preserveDrawingBuffer: true }}
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
     >
-      <OrbitControls
-        enablePan={!isMobile} // Allow pan on non-mobile devices
-        enableZoom={!isMobile} // Allow zoom on non-mobile devices
-        maxPolarAngle={Math.PI / 2}
-        minPolarAngle={Math.PI / 2}
-      />
       <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enablePan={false}
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
         <Computers isMobile={isMobile} />
       </Suspense>
       <Preload all />
     </Canvas>
   );
 };
-
 export default ComputersCanvas;
