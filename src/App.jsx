e78782a0ff8bbaebdc09868250451c78b728d7ba
jesus is king <fadi.b.alhabib@gmail.com>
2025-06-13 11:52:00 +0300

    toasts

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import {
   About,
   Contact,
-  Hero,
   Experience,
+  Hero,
   Navbar,
+  StarsCanvas,
   Tech,
   Works,
-  StarsCanvas,
-} from './components';
+} from "./components";
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
         <div className="relative z-0 bg-primary">
+          <ToastContainer />
           <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
             <Navbar />
             <Hero />
